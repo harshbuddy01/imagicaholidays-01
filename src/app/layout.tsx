373829e74fdeb2b79c340ebdc779eb3f8cd9c2fe
HARSH ANAND <harshanand@HARSHs-MacBook-Air.local>
2026-06-05 06:51:31 +0530

    feat: Add Google Analytics (GA4) integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -108,6 +108,25 @@ export default function RootLayout({
 
         {/* Floating Action Menu */}
         <ContactMenu />
+        {/* Google Analytics (GA4) */}
+        {process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID && (
+          <>
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+              strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}', {
+                  page_path: window.location.pathname,
+                });
+              `}
+            </Script>
+          </>
+        )}
 
         {/* Chatwoot Live Chat SDK */}
         <Script id="chatwoot-js" strategy="lazyOnload">
